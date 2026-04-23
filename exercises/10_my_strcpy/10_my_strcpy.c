488068f1ad01280a28a808e981d9abbd7fcf9697
1 <1@a.com>
2026-04-23 11:51:50 +0800

    10

--- exercises/10_my_strcpy/10_my_strcpy.c
+++ exercises/10_my_strcpy/10_my_strcpy.c
@@ -3,11 +3,14 @@
 int main() {
     char source[] = "Hello, World! This is a test string for my_strcpy function.\n";
     char destination[101];
-    
+
 	// TODO: 在这里添加你的代码
     // I AM NOT DONE
-    
+    for(int i=0;i<strlen(source);i++){
+        destination[i]=source[i];
+    }
+    destination[strlen(source)]=0;
     printf("拷贝后的字符串: %s", destination);
-    
+
     return 0;
 }
